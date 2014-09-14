53c58fbfe270864b49713dd8a86bfc9d29080179
Baptiste MORIN <morin.baptiste@gmail.com>
2014-09-14 18:07:26 +0200

    feat(complex): first complex implementation

---
+++ complex/src/main/scala/org/bm/maths/complex/Complex.scala
@@ -0,0 +1,101 @@
+package org.bm.maths.complex
+
+/*
+ * Scala math library.
+ * Copyright (C) 2014  Baptiste MORIN
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import java.lang.Math.{pow, sqrt}
+
+/**
+ * .
+ * @author Baptiste Morin
+ */
+class Complex(_real: Double, _imag: Double) extends Ordered[Complex] {
+  lazy val real = _real
+
+  lazy val imag = _imag
+
+  private val modulus = sqrt(pow(real, 2) + pow(imag, 2))
+
+  def unary_! = modulus
+
+  def +(that: Complex): Complex = Complex(real + that.real, imag + that.imag)
+
+  def +(that: Int): Complex = this + Complex(that)
+
+  def unary_- : Complex = Complex(-real, -imag)
+
+  def unary_~ : Complex = Complex(real, -imag)
+
+  def -(that: Complex): Complex = this + -that
+
+  def *(that: Complex): Complex = Complex(real * that.real - imag * that.imag, real * that.imag + imag * that.real)
+
+  def /(that: Complex): Complex = {
+    val a = real
+    val b = imag
+    val c = that.real
+    val d = that.imag
+
+    Complex(
+      (a * c + b * d) / (c * c + d * d), (b * c - a * d) / (c * c + d * d)
+    )
+  }
+
+  def isZero = real == 0.0 && imag == 0.0
+
+  def reciprocal = if (isZero) throw new ArithmeticException(s"$this is zero. Can not divide by zero") else Complex(1) / this
+
+
+  override def equals(that: scala.Any): Boolean = that match {
+    case t: Complex => this.compare(t) == 0
+    case _ => false
+  }
+
+  override def toString = {
+    val sb = new StringBuilder
+    if (real != 0) {
+      sb ++= s"$real"
+    }
+    if (imag < 0)
+      sb ++= s"$imag*i"
+    else if (imag > 0) {
+      if (real != 0) {
+        sb ++= "+"
+      }
+      sb ++= s"$imag*i"
+    }
+
+    sb.toString()
+  }
+
+  override def compare(that: Complex): Int = !this compareTo !that
+}
+
+object Complex {
+  val i = Complex(0, 1)
+
+  def apply(real: Double): Complex = new Complex(real, 0)
+  def apply(real: Double, imag: Double): Complex = new Complex(real, imag)
+
+  object Implicits {
+    implicit def fromDoubleToComplex(real: Double): Complex = Complex(real)
+
+  }
+
+}
---
+++ complex/src/test/scala/org/bm/maths/complex/SComplexFunTest.scala
@@ -0,0 +1,82 @@
+package org.bm.maths.complex
+
+/*
+ * Scala math library.
+ * Copyright (C) 2014  Baptiste MORIN
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import org.bm.maths.complex.Complex.Implicits._
+import org.bm.maths.complex.Complex.i
+import org.scalatest.FunSuite
+
+/**
+ * .
+ * @author Baptiste Morin
+ */
+class SComplexFunTest extends FunSuite {
+  test("org.bm.maths.complex.SComplex(1)") {
+    val c: Complex = Complex(1)
+    assert(c !== null)
+  }
+
+  test("i") {
+    assert(i.toString === Complex(0, 1).toString)
+  }
+
+  test("toString") {
+    assert((7 + 5 * i).toString === "7.0+5.0*i")
+  }
+
+  test("addition") {
+    val c1 = Complex(7, 5)
+    val c2 = Complex(8, -3)
+    println(s"c1 = $c1")
+    println(s"c2 = $c2")
+
+    val expected = Complex(15, 2)
+
+    assert(expected == c1 + c2)
+    assert(expected === c1 + c2)
+
+  }
+
+  test("modulus") {
+    val c = Complex(3, 4)
+    assert(!c === 5)
+  }
+
+  test("i^2") {
+    val r = i * i
+    val e = -1.0
+    assert(i * i === Complex(-1.0))
+  }
+
+  test("operators") {
+    val c = 7 + 5 * i
+    val expected = Complex(7, 5)
+
+    assert(expected === c)
+  }
+
+  test("reciprocal") {
+    val c = 1 + 2 * i
+    val expected = 0.2 - 0.4 * i
+
+    assert(expected === c.reciprocal)
+  }
+
+}
